ecab725da695efdbb79348d345aee5bc6ee67988
Vikas Naidu <vikas.naidu@cricbuzz.com>
2018-04-12 13:12:13 +0530

    bootstrapping

---
+++ src/com/headfirst/designpatterns/Main.kt
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 12/4/2018
+ * @author: vikas.naidu
+ */
+
+package com.headfirst.designpatterns
+
+fun main(args: Array<String>){
+    println("Head first design patterns using kotlin.")
+}
